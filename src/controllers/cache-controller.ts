c6dcb92f470d45c22432a1c544f81755ee97d528
Saheed Ajibulu <saheedajibulu@Saheeds-MacBook-Pro.local>
2020-10-31 17:42:24 +0100

    - fix bugs in fetchCacheItem method in CacheController - add  updateCacheItem method in CacheController - add  deleteCacheItem method in CacheController - add  deleteCacheItems method in CacheController

--- src/controllers/cache-controller.ts
+++ src/controllers/cache-controller.ts
@@ -3,23 +3,26 @@ import { Cache } from '../database/model/cache';
 import { NotFoundError } from '../errors';
 import { Helper, constants } from '../utils';
 
-const { generateKey, computeTTL } = Helper;
-const { statusCodes } = constants;
+const { generateKey } = Helper;
+const { statusCodes, config } = constants;
 
 export default class CacheController {
   async fetchCacheItem(req: Request, res: Response) {
     try {
       const { key } = req.params;
       const cacheItem = await Cache.findOne({ key });
-      const expired = cacheItem?.isExceededTTL();
-      if (expired) {
-        const ttl = computeTTL();
+      const expired = await cacheItem?.isExceededTTL();
+
+      if (!cacheItem || expired) {
+        console.log('cache miss');
         const newCaheItem = Cache.build({
           key,
-          ttl,
+          ttl: config.TTL,
         });
-        return res.status(statusCodes.Created).send({ cache: newCaheItem });
+        await newCaheItem.save();
+        return res.status(statusCodes.Created).send({ key: newCaheItem.key });
       }
+      console.log('cache hit');
       res.send({ cache: cacheItem });
     } catch (error) {
       throw error;
@@ -32,7 +35,8 @@ export default class CacheController {
       if (!caches) {
         throw new NotFoundError('Not records found');
       }
-      res.status(statusCodes.OK).send({ caches });
+      const keys = caches.map((cache) => cache.key);
+      res.status(statusCodes.OK).send({ keys });
     } catch (error) {
       throw error;
     }
@@ -41,14 +45,58 @@ export default class CacheController {
     try {
       const { team, country } = req.body;
       const key = generateKey();
-      const ttl = computeTTL();
       const newCacheitem = Cache.build({
         key,
         data: { team, country },
-        ttl,
+        ttl: config.TTL,
       });
       await newCacheitem.save();
       res.status(statusCodes.Created).send({ cache: newCacheitem });
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updateCacheItem(req: Request, res: Response) {
+    try {
+      const { key, team, country } = req.body;
+
+      const updateRecord = { data: { team, country } };
+
+      const foundCache = await Cache.findOneAndUpdate(
+        { key },
+        { $set: updateRecord },
+        { new: true, upsert: false }
+      );
+      if (!foundCache) {
+        throw new NotFoundError('Item not found');
+      }
+      const updatedCache = await foundCache?.save();
+
+      res.status(statusCodes.OK).send({ cache: updatedCache });
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deleteCacheItem(req: Request, res: Response) {
+    try {
+      const { key } = req.params;
+      const item = await Cache.findOneAndDelete({ key });
+      if (!item) {
+        throw new NotFoundError('Item specified for delete not found');
+      }
+      res.status(statusCodes.OK).send({ cache: item });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteCacheItems(req: Request, res: Response) {
+    try {
+      const items = await Cache.deleteMany({});
+      if (!items.deletedCount) {
+        throw new NotFoundError('No items found to delete');
+      }
+      res.status(statusCodes.OK).send({ caches: [] });
     } catch (error) {
       throw error;
     }
