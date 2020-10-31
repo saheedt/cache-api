f7a855ebede366e46ab4f46e811d3d8577f23090
Saheed Ajibulu <saheedajibulu@Saheeds-MacBook-Pro.local>
2020-10-31 17:45:51 +0100

    - add updateCacheItemSchema and deleteCacheItemSchema request validation schemas

--- src/middlewares/validation-schemas/cache.ts
+++ src/middlewares/validation-schemas/cache.ts
@@ -7,6 +7,8 @@ interface ValidationType {
 interface ValidationSchemaType {
   fetchCacheItemSchema: ValidationType;
   createCacheItemSchema: ValidationType;
+  updateCacheItemSchema: ValidationType;
+  deleteCacheItemSchema: ValidationType;
 }
 
 export const cacheValidations: ValidationSchemaType = {
@@ -36,6 +38,42 @@ export const cacheValidations: ValidationSchemaType = {
       escape: true,
       trim: true,
       errorMessage: 'A valid cache item team country is required',
+    },
+  },
+  updateCacheItemSchema: {
+    key: {
+      in: ['body'],
+      notEmpty: true,
+      isString: true,
+      escape: true,
+      trim: true,
+      errorMessage: 'A valid cache item identifier is required',
+    },
+    team: {
+      in: ['body'],
+      notEmpty: true,
+      isString: true,
+      escape: true,
+      trim: true,
+      errorMessage: 'A valid cache item team name is required',
+    },
+    country: {
+      in: ['body'],
+      notEmpty: true,
+      isString: true,
+      escape: true,
+      trim: true,
+      errorMessage: 'A valid cache item team country is required',
+    },
+  },
+  deleteCacheItemSchema: {
+    key: {
+      in: ['params'],
+      notEmpty: true,
+      isString: true,
+      escape: true,
+      trim: true,
+      errorMessage: 'A valid cache item identifier is required',
     },
   },
 };
