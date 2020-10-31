b98790ed4577ee99883701c1b9d2ed46685d6f1a
Saheed Ajibulu <saheedajibulu@Saheeds-MacBook-Pro.local>
2020-10-31 13:59:00 +0100

    - wireup mongoose and mongodb - connect routes to webserver

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import { indexRouter } from './routes';
 import { errorHandler } from './middlewares';
 import { RouteNotFoundError } from './errors';
 
 const app = express();
 
 app.use(json());
+app.use(indexRouter);
 
 app.all('*', (req: Request, res: Response) => {
   throw new RouteNotFoundError();
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import { app } from './app';
-import { Radix } from './utils/constants';
+import { constants } from './utils/';
 
 dotenv.config();
 
+const { Radix } = constants;
+
 const port = normalizePort(process.env.PORT!);
+
+const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined.');
+  }
+  if (!process.env.TTL) {
+    throw new Error('TTL must be defined.');
+  }
+
+  try {
+    console.log('MONGO_URI:', process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('connected to mongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 app.listen(port, () => {
   console.log(`Listening on Port ${port}!!!`);
 });
+
+start();
 
 function normalizePort(portNumber: string): number {
   const defaultPortNumber = 5000;
